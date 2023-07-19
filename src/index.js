907cf18d04f5c8d0bcb91414cd6fe857b5ddec40
hectorjiang <jiangyongxu1208@gmail.com>
2023-07-19 19:21:04 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+/*
+ * @Author: Hector Jiang
+ * @Date: 2023-07-16 12:26:32
+ * @LastEditTime: 2023-07-19 18:31:09
+ * @Description: 
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
+import Todo from "./pages/Todo/Todo"
 import reportWebVitals from './reportWebVitals';
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/todo",
+        element: <Todo />
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Todo/Todo.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: Hector Jiang
+ * @Date: 2023-07-18 21:01:35
+ * @LastEditTime: 2023-07-19 18:58:20
+ * @Description: 
+ */
+import Button from "@mui/material/Button"
+export default function Todo() {
+    return (
+        <div>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    )
+}
